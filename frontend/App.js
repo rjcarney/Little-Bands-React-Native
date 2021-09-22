fdfa5a8cc01fe971504b2d1423119e7bd5790421
rjcarney <33989142+rjcarney@users.noreply.github.com>
2021-09-22 13:10:40 -0400

    Project Setup
    Expo: Blank Javascript
    Firebase: Authentication, Firestore Database, Realtime Database, Storage, Functions

--- frontend/App.js
+++ frontend/App.js
@@ -2,10 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Constants } from 'expo-constants';
+import firebase from 'firebase/app';
+
+if(firebase.apps.length == 0) {
+  firebase.initializeApp(Constants.manifest.web.config.firebase);
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to Little Bands!</Text>
       <StatusBar style="auto" />
     </View>
   );
