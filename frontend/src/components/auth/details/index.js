cf52b5737fb3cb3bd4b054b85a459dbec600d06e
rjcarney <33989142+rjcarney@users.noreply.github.com>
2021-09-23 13:39:33 -0400

    Firebase Authentication
    Users can register saving credentials to database and login by entering valid credentials.

--- frontend/src/components/auth/details/index.js
+++ frontend/src/components/auth/details/index.js
@@ -2,10 +2,32 @@ import React, { useState } from 'react'
 import { View, Text, TouchableOpacity, TextInput } from 'react-native'
 import { Feather } from '@expo/vector-icons';
 import styles from './styles';
+import { useDispatch } from 'react-redux';
+import { login, register } from '../../../redux/actions'
 
 export default function AuthDetails({ authPage, setDetailsPage }) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const dispatch = useDispatch()
+    const handleLogin = () => {
+        dispatch(login(email, password))
+            .then(() => {
+                console.log('login successful')
+            })
+            .catch(() => {
+                console.log('login unsuccessful')
+            })
+    }
+    const handleRegister = () => {
+        dispatch(register(email, password))
+            .then(() => {
+                console.log('register successful')
+            })
+            .catch(() => {
+                console.log('register unsuccessful')
+            })
+    }
 
     return (
         <View style={styles.container}>
@@ -21,9 +43,11 @@ export default function AuthDetails({ authPage, setDetailsPage }) {
             <TextInput
              onChangeText={(text) => setPassword(text)}
              style={styles.textInput}
+             secureTextEntry
              placeholder='Password'
             />
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button}
+            onPress={() => authPage == 0 ? handleLogin() : handleRegister()}>
                 <Text style={styles.buttonText}>{authPage == 0 ? 'Sign In' : 'Sign Up'}</Text>
             </TouchableOpacity>
         </View>
---
+++ frontend/src/redux/actions/auth.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase';
+requestAnimationFrame('firebase/firebase-auth');
+
+export const login = (email, password) => dispatch => new Promise((resolve, reject) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(() => {
+            resolve()
+        })
+        .catch(() => {
+            reject()
+        })
+})
+
+export const register = (email, password) => dispatch => new Promise((resolve, reject) => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(() => {
+            resolve()
+        })
+        .catch(() => {
+            reject()
+        })
+})
